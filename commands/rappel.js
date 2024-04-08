de3bf1f1ace8d2e1dc08dcbc604246c2caa1222d
Mario Mathonnat <mariomathonnat116@gmail.com>
2024-04-08 18:42:59 +0200

    test

--- commands/rappel.js
+++ commands/rappel.js
@@ -1,30 +1,22 @@
-const { MessageEmbed } = require("discord.js");
 const schedule = require("node-schedule");
 
 module.exports = (bot) => {
-  async function planifierMessageVendrediSoir() {
-    schedule.scheduleJob("27 18 * * 1", async () => {
+  function planifierMessageVendrediSoir() {
+    // Planifie le job pour s'exécuter à 18h30 chaque lundi
+    schedule.scheduleJob("47 18 * * 1", async () => {
       try {
         const channel = await bot.channels.fetch("1148182103989698642");
-
-        // Création de l'embed
-        const embed = new MessageEmbed()
-          .setTitle("Jeu du mois tous les vendredis !")
-          .setDescription(
-            "Tous les vendredis du mois d'avril accueilleront un among-us à 21h, n'hésitez pas à venir !"
-          )
-          .setColor(0x0aee1a)
-          .setTimestamp();
-
-        // Envoi de l'embed
-        await channel.send({ embeds: [embed] });
+        // Envoie un message texte classique
+        await channel.send(
+          "Tous les vendredis du mois d'avril accueilleront un among-us à 21h, n'hésitez pas à venir !"
+        );
       } catch (error) {
         console.error("Erreur lors de l'envoi du message :", error.toString());
       }
     });
 
     console.log(
-      "Message automatique du vendredi soir planifié pour chaque vendredi à 20h00."
+      "Message automatique pour le vendredi soir planifié pour chaque lundi à 18h30."
     );
   }
 
