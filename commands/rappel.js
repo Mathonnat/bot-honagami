2ecb512cf6f8706b99ab02ab31c1174618bacb77
Mario Mathonnat <mariomathonnat116@gmail.com>
2024-04-08 17:44:17 +0200

    test

--- commands/rappel.js
+++ commands/rappel.js
@@ -3,7 +3,7 @@ const schedule = require("node-schedule");
 
 module.exports = (bot) => {
   async function planifierMessageVendrediSoir() {
-    schedule.scheduleJob("33 17 * * 1", async () => {
+    schedule.scheduleJob("48 17 * * 1", async () => {
       try {
         const channel = await bot.channels.fetch("1148182103989698642");
 
